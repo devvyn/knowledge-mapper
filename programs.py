08ccac29169e7b06e325023699b1b8d8229b9689
Devvyn Patrick Murphy <devvyn@devvyn.io>
2019-09-16 14:13:27 -0600

    :recycle: refactor: extract string constant
    usask programs base web URL

--- programs.py
+++ programs.py
@@ -1,7 +1,9 @@
 from urllib.parse import urljoin
 
+PROGRAMS_USASK_CA_ = 'https://programs.usask.ca/'
+
 
 def programs_url(url_path):
-    data_page_url_base = 'https://programs.usask.ca/'
+    data_page_url_base = PROGRAMS_USASK_CA_
     url = urljoin(data_page_url_base, url_path)
     return url
