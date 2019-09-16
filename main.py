3993649bdae1b95d08534fb54d028d13189ae2a7
Devvyn Patrick Murphy <devvyn@devvyn.io>
2019-09-16 14:14:50 -0600

    :construction: outlined remaining code tasks in notebook main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # %%
 """
-fn get list of course details:
+fn get list of course details data:
     get list of program types urls
     for program type details page in program type urls:
         get list of program urls
@@ -8,31 +8,64 @@ fn get list of course details:
             get list of courses urls
             for course details page in courses urls:
                 get course details data
-                (add to dependency graph)
 """
 
 # %%
 import html_helper
 from programs import programs_url
 
-BIOINFORMATICS_INDEX_PHP = 'arts-and-science/bioinformatics/index.php'
-
 """
-> get list of programs
+> get list of program types urls
 """
 
-# DATA_PROGRAMS_LIST_PAGE_URL_ = "https://programs.usask.ca/programs/list-of-programs.php"
-
-
-programs_links = html_helper.get_links_from_page(programs_url(BIOINFORMATICS_INDEX_PHP))
+PROGRAM_TYPES_LIST_PAGE_PATH = "programs/list-of-programs.php"
+program_types_list_page_url = programs_url(PROGRAM_TYPES_LIST_PAGE_PATH)
+program_types_links = html_helper.get_links_from_page(program_types_list_page_url)
 
 # 212 programs
 
-# EXAMPLE_PROGRAM = 'Bioinformatics'
-# program_page_url = program_dict[program]
+# %%
+
+
+"""
+> for program type details page in program type urls:
+>     get list of program urls
+"""
+
+# temporary bypass of loop
+PROGRAM_TYPE_PAGE_PATH_BIOINFORMATICS = 'arts-and-science/bioinformatics/index.php'  # @todo: delete this line ASAP
+program_type_page_path = PROGRAM_TYPE_PAGE_PATH_BIOINFORMATICS
+
+# to be put inside for loop:
+program_type_page_url = programs_url(program_type_page_path)
+program_links = html_helper.get_links_from_page(program_type_page_url)
 
 # %%
 
 """
-> get list of courses
+>         for program details page in program urls:
+>             get list of courses urls
 """
+
+# temp bypass of loop
+PROGRAM_PAGE_PATH_BS4Y_BINF = "bsc-4-bioinformatics.php"
+program_page_path = PROGRAM_PAGE_PATH_BS4Y_BINF
+
+# inside for loop:
+program_page_url = programs_url(program_page_path)
+courses_links = html_helper.get_links_from_page(program_page_url)
+
+# %%
+
+"""
+>             for course details page in courses urls:
+>                 get course details data
+"""
+
+# temp bypass
+COURSE_PAGE_PATH_EXAMPLE = ""  # @todo: fill
+course_page_path = COURSE_PAGE_PATH_EXAMPLE
+
+# inside for loop:
+# course_page_url = courses_url(course_page_path)
+# get course details data
