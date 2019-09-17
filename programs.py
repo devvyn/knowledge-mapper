5be2e3a1a165d7bee06f678bbc3fbe998ac388e3
Devvyn Patrick Murphy <devvyn@devvyn.io>
2019-09-17 15:47:22 -0600

    :bulb: add todos about absolute URL resolution

--- programs.py
+++ programs.py
@@ -8,6 +8,7 @@ def fetch_subjects_by_study_level(subjects_list_page_url=LIST_OF_PROGRAMS):
     program_subjects_by_study_level = {
         match.etree_element.text.strip(): {
             sub_match.etree_element.text.strip(): sub_match.etree_element.attrib['href']
+            # @todo: resolve absolute URLs ASAP
             for sub_match in match.parent.query_all('li>a')}
         for match in (root.query_all('section.uofs-section h1'))}
     return program_subjects_by_study_level
@@ -17,5 +18,6 @@ def fetch_programs_by_subject(program_subject_page_url):
     root = fetch_wrapped_root_cssselect2(program_subject_page_url)
     programs_in_subject = {
         element.etree_element.text: element.etree_element.attrib['href']
+        # @todo: resolve absolute URLs ASAP
         for element in root.query_all('section#Programs ul>li>a')}
     return programs_in_subject
