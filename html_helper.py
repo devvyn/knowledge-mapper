4ce727814c564fb93f24456baa37f50814348aa8
Devvyn Patrick Murphy <devvyn@devvyn.io>
2019-09-17 13:29:48 -0600

    :recycle: refactor to optimize imports

--- html_helper.py
+++ html_helper.py
@@ -1,4 +1,4 @@
-import xml.etree
+import xml
 
 import cssselect2
 import html5lib
@@ -16,6 +16,19 @@ def get_page_html(page_url):
     return page_html
 
 
-def get_page_etree(page_html: str):
-    # return xml.etree.ElementTree.fromstring(page_html)
+def parse_html(page_html: str) -> xml.etree.ElementTree:
     return html5lib.parse(page_html)
+
+
+def fetch_page_etree(page_url) -> xml.etree.ElementTree:
+    return parse_html(get_page_html(page_url))
+
+
+def wrap_etree_cssselect2(page_etree):
+    root = cssselect2.ElementWrapper.from_html_root(page_etree)
+    return root
+
+
+def fetch_wrapped_root_cssselect2(url):
+    root = wrap_etree_cssselect2(fetch_page_etree(url))
+    return root
--- programs.py
+++ programs.py
@@ -1,21 +1,13 @@
-import cssselect2
+from html_helper import fetch_wrapped_root_cssselect2
 
-import html_helper
+LIST_OF_PROGRAMS = "https://programs.usask.ca/programs/list-of-programs.php"
 
 
-def get_subjects_by_study_level():
-    program_subjects_list_page_html = html_helper.get_page_html(program_subjects_list_page_url())
-    program_subjects_list_page_tree = html_helper.get_page_etree(program_subjects_list_page_html)
-    section_query = 'section.uofs-section h1'
-    root = cssselect2.ElementWrapper.from_html_root(program_subjects_list_page_tree)
-    study_level_sections_selection = root.query_all(section_query)
+def get_subjects_by_study_level(subjects_list_page_url=LIST_OF_PROGRAMS):
+    root = fetch_wrapped_root_cssselect2(subjects_list_page_url)
     program_subjects_by_study_level = {
         match.etree_element.text.strip(): {
             sub_match.etree_element.text.strip(): sub_match.etree_element.attrib['href']
             for sub_match in match.parent.query_all('li>a')}
-        for match in study_level_sections_selection}
+        for match in (root.query_all('section.uofs-section h1'))}
     return program_subjects_by_study_level
-
-
-def program_subjects_list_page_url():
-    return "https://programs.usask.ca/programs/list-of-programs.php"
