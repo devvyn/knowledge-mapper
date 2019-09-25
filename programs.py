70dfdd80ff52b9677dc4e767c956cae8fea272b3
Devvyn Patrick Murphy <devvyn@devvyn.io>
2019-09-25 15:54:28 -0600

    fix(programs.py): correct method call

--- programs.py
+++ programs.py
@@ -1,5 +1,5 @@
 from html_helper import (fetch_cssselect2_root, abs_url, reformat_text,
-                         get_href)
+                         get_href, get_reformatted_text)
 
 
 def fetch_programs_by_subject(page_url: str) -> dict:
@@ -7,7 +7,7 @@ def fetch_programs_by_subject(page_url: str) -> dict:
     links_in_programs_section = 'section#Programs ul>li>a'
     links = root.query_all(links_in_programs_section)
     programs_in_subject = {
-        reformat_text(element):
+        get_reformatted_text(element):
             abs_url(page_url, get_href(element))
         for element in links}
     return programs_in_subject
