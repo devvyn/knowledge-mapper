33668ba7d6d131afc3534da1817908dd306d7944
Devvyn Patrick Murphy <devvyn@devvyn.io>
2019-09-25 22:58:46 -0600

    test(test_programs_by_subject): write BDD tests

--- test/step_defs/test_programs_by_subject.py
+++ test/step_defs/test_programs_by_subject.py
@@ -1,4 +1,6 @@
-from pytest_bdd import given, then, scenario
+from pytest_bdd import given, scenario, then
+
+import programs
 
 
 @scenario('../features/programs_by_subject.feature',
@@ -9,11 +11,20 @@ def test_programs_by_subject():
 
 @given("the UofS <study_level> programs for <subject>")
 def program_dict_for_subject(study_level, subject):
-    raise NotImplementedError(
-        u'STEP: Given the UofS program list for <subject>')
+    return programs.fetch_programs_by_subject('https://programs.usask.ca/'
+                                              'arts-and-science/'
+                                              'bioinformatics/index.php')
 
 
-@then("there is at least 1 degree program link")
-def there_is_at_least_1_degree_program_link():
-    raise NotImplementedError(
-        u'STEP: Then there is at least 1 degree program link')
+@then("there is at least 1 degree program")
+def there_is_at_least_1_degree_program_link(program_dict_for_subject):
+    assert len(program_dict_for_subject) >= 1
+
+
+@then("all degree programs have at least 1 link")
+def all_degree_programs_have_min_links(program_dict_for_subject):
+    assert all((
+        value.startswith('http')
+        for value
+        in program_dict_for_subject.values()
+    ))
