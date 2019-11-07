a6599b53695a0e10e20e3d9ad73db91e3de7254a
devvyn <devvyn@devvyn.io>
2019-11-07 15:54:19 -0600

    feat(file_cache, fetch): :sparkle: automatically create cache dir
    In order to initialize the cache easily, a cache instance is used
      instead of static module. This also allows state to be managed within
      the module.

--- scrape/file_cache.py
+++ scrape/file_cache.py
@@ -1,5 +1,8 @@
 import pathlib
 import re
+import sys
+from os import makedirs
+from os.path import isdir
 from typing import Union
 
 from scrape.url import fill_url
@@ -11,7 +14,7 @@ CACHE_PATH: str = './data/'  # relative to working directory
 PATH = Union[pathlib.Path, str]
 
 
-def get(key: str):
+def get(key: str) -> str:
     with open(get_cache_path(key), MODE_READ) as f:
         return f.read()
 
@@ -44,3 +47,42 @@ def strip_invalid_characters(filename: str) -> str:
 
 def replace_spaces(string: str) -> str:
     return string.strip().replace(' ', '_')
+
+
+def init_cache(cache_dir: str) -> str:
+    resolved_path = pathlib.Path(cache_dir).resolve()
+    try:
+        makedirs(resolved_path, exist_ok=True)
+    except FileExistsError:
+        # if this causes error, there's a file with the same name already
+        print(f"File exists at {resolved_path}.", file=sys.stderr)
+    if not isdir(resolved_path):
+        raise NotADirectoryError(resolved_path)
+    return resolved_path
+
+
+class WebCache:
+    dir: Union[str, pathlib.Path]
+    def __init__(self, cache_dir: str = CACHE_PATH):
+        self.dir = init_cache(cache_dir)
+
+    def get(self, *args, **kwargs):
+        return get(*args, **kwargs)
+
+    def getdefault(self, *args, **kwargs):
+        return getdefault(*args, **kwargs)
+
+    def get_valid_filename(self, *args, **kwargs):
+        return get_valid_filename(*args, **kwargs)
+
+    def get_cache_path(self, *args, **kwargs):
+        return get_cache_path(*args, **kwargs)
+
+    def url_to_filename(self, *args, **kwargs):
+        return url_to_filename(*args, **kwargs)
+
+    def strip_invalid_characters(self, *args, **kwargs):
+        return strip_invalid_characters(*args, **kwargs)
+
+    def replace_spaces(self, *args, **kwargs):
+        return replace_spaces(*args, **kwargs)
