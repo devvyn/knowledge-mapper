5894c59aea76ebbc0862f75507e9e096eddeeac0
Devvyn P M <devvyn@devvyn.io>
2019-11-15 23:38:55 -0600

    docs(fetch): add docstring

--- src/scrape/fetch.py
+++ src/scrape/fetch.py
@@ -5,4 +5,10 @@ from scrape.file_cache import cached
 
 @cached
 def get_content(url: str) -> str:
+    """
+    Fetch web page content from given URL
+
+    :param url:
+    :return:
+    """
     return requests.get(url).text
