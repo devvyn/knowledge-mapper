b02b9701270f6d0625867d8caf7b3c42708138eb
Devvyn P M <devvyn@devvyn.io>
2019-11-24 20:42:54 -0600

    feat(usage_examples): add program name parser

--- usage_examples.py
+++ usage_examples.py
@@ -1,4 +1,5 @@
 # %%
+
 from scrape.model.program_catalogue import ProgramCatalogue
 
 catalogue = ProgramCatalogue()
@@ -18,9 +19,55 @@ undergrad_binf = undergrad['Bioinformatics']
 undergrad_binf
 
 # %%
+import re
+from typing import Tuple
+from logging import debug
 
-from scrape.parse import clean_whitespace, parse_program
-from scrape.page.courses_in_program import get_program_page
+keys = list(
+    (level, field, program,)
+    for level in catalogue
+    for field in catalogue[level]
+    for program in catalogue[level][field]
+)
+
+
+def program_explode(program: str) -> Tuple[str, str]:
+    # @todo: data object
+    program, field = program.split(' - ')
+    debug(program, field)
+    program_long, program_short = re.match(
+        r'(?P<long>.+)(?: \((?P<short>.+)\))',
+        program,
+    ).group('long', 'short', )
+    debug(program_long, program_short)
+    return field, program_long, program_short
+
+
+flattened_catalogue = {
+    (level, field, program_short):
+        [
+            program_long,
+            catalogue[level][field][program],
+        ]
+    for (level, field, (program_long, program_short,), program,)
+    in (
+        (level, field, program_explode(program), program,)
+        for (level, field, program)
+        in keys
+    )
+}
+
+# %%
+
+import pandas
+
+src = pandas.DataFrame.from_dict(
+    data=flattened_catalogue,
+    orient='index',
+    columns=['program', 'URL']
+)
+
+# %%
 
 level = 'Undergraduate'
 field = 'Bioinformatics'
@@ -31,7 +78,7 @@ data = parse_program(content)
 data
 
 # %%
-from scrape.fetch import get_content
+
 from scrape.parse import get_program_data
 
 # FIXME get fails, not implemented
