738f57a6a15c1bf5d27fce219b01ba2739717f68
Devvyn P M <devvyn@devvyn.io>
2019-11-27 17:13:11 -0600

    WIP: make data fetch/generation lazy :construction:

--- src/devvyn/scrape/model/program_catalogue.py
+++ src/devvyn/scrape/model/program_catalogue.py
@@ -7,52 +7,54 @@ import devvyn.scrape.page.fields_at_levels
 # @todo: implement tests
 
 
-class ProgramCatalogue(scrape.model.core.SourceMapping):
+class ProgramCatalogue(devvyn.scrape.model.core.SourceMapping):
     """
     Represent scraped data, with the list of study levels at the root and
     courses as the leaf nodes
     """
 
-    # @todo: make lazy
-    # @todo: make separate dataclass and logic class
     def __init__(self, src: str = None) -> None:
         if src is not None:
             url_ = str(src)
         else:
-            url_ = scrape.page.fields_at_levels.get_fields_url()
+            url_ = devvyn.scrape.page.fields_at_levels.get_fields_url()
         super().__init__(url_)
-        root = scrape.page.fields_at_levels.get_all_fields(self.src)
-        # self.data = root
+        study_fields = devvyn.scrape.page.fields_at_levels.get_all_fields(self.src)
         self.data = {
-            level: {
-                field: Field(level, field, src, ) for field, src in
-                field_map.items()
-            }
-            for level, field_map in root.items()
+            level: StudyLevel(name=level, data=field_map,
+                              src=self.src)
+            for level, field_map in study_fields.items()
         }
 
 
-class Field(scrape.model.core.NamedSource):
+class StudyLevel(devvyn.scrape.model.core.NamedSource):
+    def __init__(self, name: str, data: dict, src: str):
+        super(StudyLevel, self).__init__(name=name, src=src)
+        level = name
+        self.data = {
+            field: Field(level, field, src, ) for field, src in data.items()
+        }
+
+
+class Field(devvyn.scrape.model.core.NamedSource):
     """ Field of study. """
 
     # @todo: make separate dataclass and logic class
     def __init__(self, level: str, name: str, src: str) -> None:
         super().__init__(name, src)
-        if level is None:
-            raise TypeError(level)
         self.level = level
 
         # @todo: use src
-        base_href = scrape.page.programs_in_field_at_level.get_programs_url(
-            level, name)
+        # base_href = scrape.page.programs_in_field_at_level.get_programs_url(
+        #     level, name)
         # @todo: make lazy
-        content = scrape.fetch.get_content(base_href)
-        self.data = scrape.page.programs_in_field_at_level.get_programs(
-            content, base_href)
+        # content = scrape.fetch.get_content(base_href)
+        # self.data = scrape.page.programs_in_field_at_level.get_programs(
+        #     content, base_href)
         self.data = {}
 
 
-class Program(scrape.model.core.NamedSource):
+class Program(devvyn.scrape.model.core.NamedSource):
     """ Program in a field of study. """
     # @todo: make lazy
     # @todo: make separate dataclass and logic class
