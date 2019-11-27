ba9eadd59bb97ff404ac102c4de1c5578658b101
Devvyn P M <devvyn@devvyn.io>
2019-11-26 23:21:20 -0600

    test: remove unnecessary files and lines

--- test/steps/test_fields_at_college.py
+++ test/steps/test_fields_at_college.py
@@ -5,28 +5,18 @@ use_step_matcher("re")
 
 @given("the list of all programs")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
+    # @todo: use ProgramCatalogue
     from scrape.page.fields_at_levels import get_all_fields
     context.lookup = get_all_fields()
 
 
 @then("the field of (?P<field>.+) is listed under (?P<level>.+)")
 def step_impl(context, field, level):
-    """
-    :type context: behave.runner.Context
-    :type level: str
-    :type field: str
-    """
     assert field in context.lookup[level], context.lookup.keys()
 
 
 @given("the web page for the list of all programs")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
     from scrape.page.fields_at_levels import get_fields_url
     base_href = get_fields_url()
     from scrape.fetch import get_content
--- test/test_url.py
+++
@@ -1,8 +0,0 @@
-from scrape.url import get_requirements_url
-
-
-def test_get_requirements_url():
-    assert get_requirements_url(
-        'Bioinformatics',
-        'Bachelor of Science Four-year (B.Sc. Four-year)'
-    ) == 'https://programs.usask.ca/arts-and-science/bioinformatics/index.php'
