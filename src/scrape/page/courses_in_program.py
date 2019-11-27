5a6f3e9489788f04629f6b11737fe80e255459c8
Devvyn P M <devvyn@devvyn.io>
2019-11-26 23:42:53 -0600

    test(courses_in_program): remove unused methods

--- src/scrape/page/courses_in_program.py
+++ src/scrape/page/courses_in_program.py
@@ -1,22 +1,6 @@
-import itertools
-from typing import Iterable, Optional
-
 from scrape.fetch import get_content
-from scrape.page.fields_at_levels import get_all_fields
 from scrape.page.programs_in_field_at_level import get_programs_url
 from scrape.parse import parse_programs
-
-
-def first_field_matching(program):
-    all_fields: dict = get_all_fields()
-    sections_children: Iterable[dict] = itertools.chain(all_fields.values())
-    fields_list = (
-        value
-        for program_pages in sections_children
-        for key, value in program_pages.items()
-        if key == program
-    )
-    return next(fields_list)
 
 
 def get_program_page(program: str, field: str, level: str) -> str:
@@ -44,43 +28,3 @@ def get_program_page(program: str, field: str, level: str) -> str:
     )
     content = get_content(program_page_url)
     return content
-
-
-def attempt_get_program_page_url(field_str, level):
-    field_candidate = field_str
-    # @todo: try get and check
-    field_candidate_url = get_programs_url(level, field_candidate)
-    field_candidate_content = get_content(
-        field_candidate_url)
-    field_candidate_data = parse_programs(field_candidate_content,
-                                          field_candidate_url)
-    if field_str in field_candidate_data.keys():
-        return field_candidate_data[field_str]
-
-
-def infer_field(program: str, sep: str = " - ") -> Optional[str]:
-    """
-    Try to find a field of study in the program name. Assume the field of
-    study is named after the given separator.
-
-    :param program: The name of the program
-    :param sep: Separator to use for splitting `program`
-    :return:
-    """
-    program_str, field_str = program.split(sep, maxsplit=2)
-    return field_str
-
-
-def infer_level(program_str):
-    bachelor = 'Bachelor'
-    undergrad = 'Undergraduate'
-    master = 'Master'
-    grad = 'Graduate'
-    phd = 'Doctor'
-    levels = {
-        bachelor: undergrad,
-        master: grad,
-        phd: grad,
-    }
-    level_str = levels[program_str.split(maxsplit=2)[0]]
-    return level_str
