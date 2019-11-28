668d4ea2cf0b3dd37d4bda5f9ea8ccfa9d8d0413
Devvyn P M <devvyn@devvyn.io>
2019-11-27 18:27:33 -0600

    refactor(scrape.model.course_catalogue): remove superclass, add docs

--- src/devvyn/scrape/model/course_catalogue.py
+++ src/devvyn/scrape/model/course_catalogue.py
@@ -1,6 +1,20 @@
-from devvyn.scrape.model.core import NamedSource
+"""
+Course catalogue model.
+
+The model must be able to:
+
+- Find official course description online, given only a course code.
+- Understand course codes in various formats:
+  - PSY 120
+  - PSY-120
+  - PSY 120.3
+  - PSY-120.3
+- Parse course prerequisite descriptions, producing:
+  - list of course codes mentioned
+  - remaining content fragments that could not be parsed
+"""
 
 
-class Course(NamedSource):
+class Course:
     """ Course in one or more programs. """
     pass
