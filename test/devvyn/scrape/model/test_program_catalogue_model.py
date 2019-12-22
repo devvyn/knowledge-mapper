c1bb569cf199f21cb8dff2eee19deccb8fc207a5
Devvyn P M <devvyn@devvyn.io>
2019-12-22 09:51:42 -0600

    test(test_program_catalogue_model): fix assertion error message

--- test/devvyn/scrape/model/test_program_catalogue_model.py
+++ test/devvyn/scrape/model/test_program_catalogue_model.py
@@ -11,17 +11,18 @@ class TestProgramCatalogue:
 
     def test_program_catalogue_contains_study_levels(self, catalogue):
         catalogue_children = catalogue.values()
-        assert len(catalogue_children) > 0, f'dictionary contains non-study level object'
+        assert len(catalogue_children) > 0, 'no children in catalogue'
         assert all(
             isinstance(child, devvyn.model.program_catalogue.AcademicLevel)
             for child in catalogue_children
-        )
+        ), f'dictionary contains non-study level object'
 
     def test_program_catalogue_all_field_pages_parsed(self, catalogue):
         assert len(catalogue) > 0
         for level, fields in catalogue.items():
             for field, programs in fields.items():
-                assert len(programs) > 0, f'empty collection at {level=}, {field=}'
+                field_report = f'empty collection at {level=}, {field=}'
+                assert len(programs) > 0, field_report
 
     @pytest.fixture
     def catalogue(self):
