23ced3d42ac56b95f0a9d737ecaaed3ed312bcc4
Devvyn P M <devvyn@devvyn.io>
2019-12-28 21:26:33 -0600

    docs(test_program_catalogue_page): clarify docstring

--- test/devvyn/scrape/page/test_program_catalogue_page.py
+++ test/devvyn/scrape/page/test_program_catalogue_page.py
@@ -3,10 +3,12 @@ import devvyn.scrape.page.usask.program_catalogue
 
 def test_program_catalogue_data() -> None:
     """
-    Verify that `devvyn.scrape.page.program_catalogue.program_catalogue_data()` returns a dict-like object that isn't
-    empty.
+    Verify that `devvyn.scrape.page.program_catalogue.program_catalogue_data()`
+    returns a dict-like object that isn't empty.
     """
-    catalogue = devvyn.scrape.page.usask.program_catalogue.program_catalogue_data()
+    catalogue = (
+        devvyn.scrape.page.usask.program_catalogue.program_catalogue_data()
+    )
 
     assert catalogue
     assert len(catalogue) > 0
