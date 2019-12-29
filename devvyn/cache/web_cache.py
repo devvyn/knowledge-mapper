5305381b45c4e2aacedfc783d03247de56560157
Devvyn P M <devvyn@devvyn.io>
2019-12-28 21:42:15 -0600

    fix(cache.web_cache): override `set` for proper key normalization

--- devvyn/cache/web_cache.py
+++ devvyn/cache/web_cache.py
@@ -1,4 +1,3 @@
-import re
 import urllib.parse
 from typing import Any
 
@@ -6,33 +5,47 @@ from typeguard import typechecked
 
 from devvyn.cache.file_cache import FileCache
 
-
-class WebCache(FileCache):
-    """
-    A file cache handler which ensures keys are consistently treated as unambiguous URLs.
-    """
-
-    @typechecked
-    def get(self, key: str) -> Any:
-        """
-        Retrieve content at `key` from cache or web.
-
-        :param key: URL used to load content from cache or fetch content from web
-        :param default: value to return instead of fetching content if `key` not found
-        :return: content retrieved, or `default` if provided and key not found
-        """
-        html_filename: str = url_to_filename(key) + ".html"
-        return super().get(html_filename)
+SEP_DOT: str = '.'  # URL component separator string
 
 
 @typechecked
-def url_to_filename(url: str) -> str:
+def filename_from_url(key: str) -> str:
     """ Parse the URL, concatenate the components, and remove any remaining
     characters not used in URL paths or filenames. This includes most
     non-alphanumeric characters, such as whitespace, most punctuation,
     and all symbols. """
-
-    as_dict = urllib.parse.urlparse(url)._asdict()
+    as_dict = urllib.parse.urlparse(key)._asdict()
     exploded = as_dict.values()
-    filename = ''.join(exploded)
-    return re.sub(r'(?u)[^-\w.]', '', filename)
+    base_name = SEP_DOT.join(exploded)
+    filename = f"{base_name}.html"
+    return filename
+
+
+class WebCache(FileCache):
+    """
+    A file cache handler which ensures keys are consistently treated as
+    unambiguous URLs.
+    """
+
+    @typechecked
+    def get(self, url: str) -> Any:
+        """
+        Retrieve content at `key` from cache or web.
+
+        :param url: URL used to load content from cache or fetch content
+        from web not found
+        :return: content retrieved
+        """
+        filename = filename_from_url(url)
+        return super().get(filename)
+
+    @typechecked
+    def set(self, url: str, text: str) -> None:
+        """
+        Save with the key converted from a URL string to a plain string.
+
+        :param url:
+        :param text:
+        """
+        filename = filename_from_url(url)
+        super().set(filename, text)
